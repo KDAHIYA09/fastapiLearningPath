504de69e8bb5b2194c0cef93f05e13854271a180
Khushbu <kdahiya296@gmail.com>
2025-09-23 15:03:16 +0530

    todo_list_example_apis

--- main.py
+++ main.py
@@ -1,22 +1,99 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/greet")
-def get_name(name : str):
-    return {"message": f"hello, {name}"}
+#todo try dummy
+
+#step 1 create a pydantic model for todo
+class Todo(BaseModel):
+    id: int
+    task: str
+    isDone: bool = False
+
+class TodoUpdate(BaseModel):
+    task: Optional[str] = None
+    isDone: Optional[bool] = None
 
 
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
+# for basic we will use in app memory
+todos = []
+
+#1st crud - c for create 
+@app.post("/todos/")
+def create_todo(todo: Todo):
+    #first check if todo already exists
+    # for loop syntax in python for iterating
+    for existing in todos:
+        if existing.id == todo.id:
+            raise HTTPException(status_code=400, detail="Todo with same id already exists!!")
+        
+    
+    todos.append(todo)
+    return{"message": "Todo created successfully", "todo": todo}
 
 
-@app.post("/users/")
-def create_user(user: User):
-    return {"message": "User created successfully", "user": user}
+#2nd crud - r for read 
+@app.get("/todos/")
+def get_todos():
+    return {"todos": todos}
+
+#2nd part b crud - r for read by id
+@app.get("/todos/{todo_id}/")
+def get_todo_by_id(todo_id: int):
+    for existing in todos:
+        if existing.id == todo_id:
+            return {"message": "Todo with specified id found successfully", "todo": existing}
+            
+    raise HTTPException(status_code=400, detail="No such item exists")
+
+
+#3rd crud - u for update 
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo: Todo):
+    for existing in todos:
+        if existing.id == todo_id:
+            existing.task = todo.task
+            existing.isDone = todo.isDone
+            return {"message": "Todo updated successfully", "todo": existing}
+    
+    raise HTTPException(status_code=400, detail="No such item exists")
+
+
+#3rd part b - crud u for update part b update partially patch example
+@app.patch("/todos/{todo_id}")
+def update_todo_partially(todo_id: int, todo_update: TodoUpdate):
+    for existing in todos:
+        if existing.id == todo_id:
+            if todo_update.task is not None:
+                existing.task = todo_update.task
+            
+            if todo_update.isDone is not None:
+                existing.isDone = todo_update.isDone
+            return {"message": "Todo updated successfully", "todo": existing}
+    
+    raise HTTPException(status_code=400, detail="No todo with specified id exists")
+
+
+
+#4 crud d for delete
+@app.delete("/todos/{todo_id}/")
+def delete_todo(todo_id: int):
+    for existing in todos:
+        if existing.id == todo_id:
+            todos.remove(existing)
+            return {"message": "Todo deleted successfully", "todo": existing}
+    
+    raise HTTPException(status_code=404, detail="No such todo exists")
+
+
+
+
+
+
+
+        
 
 
 
@@ -25,6 +102,21 @@ def create_user(user: User):
 
 
 
+
+
+
+# @app.get("/greet")
+# def get_name(name : str):
+#     return {"message": f"hello, {name}"}
+
+# class User(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+
+# @app.post("/users/")
+# def create_user(user: User):
+#     return {"message": "User created successfully", "user": user}
 
 # @app.get("/")
 # def read_root():
